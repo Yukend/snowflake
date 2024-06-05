59f8c3b1883119168b93f545aaf82ea171e0a4af
Yukend <kyukendiran16@gmail.com>
2024-06-05 12:43:21 +0530

    completed draft

---
+++ ORDER_PROCESS/Tables/CUSTOMER.sql
@@ -0,0 +1,72 @@
+CREATE 
+OR REPLACE TABLE ORDER_BASE.T_CUSTOMER (
+  CUSTOMER_ID VARCHAR(100), 
+  ADDRESS VARCHAR(500), 
+  PHONE_NO VARCHAR(100), 
+  CATEGORY VARCHAR(100), 
+  STATUS VARCHAR(100), 
+  TOTAL_ORDERS INTEGER DEFAULT 0, 
+  TOTAL_INVOICE_AMOUNT NUMBER(12, 2) DEFAULT 0, 
+  LOYALTY_POINTS INTEGER DEFAULT 0, 
+  CREATE_DATE DATE, 
+  CREATE_USER VARCHAR(100)
+);
+INSERT INTO ORDER_BASE.T_CUSTOMER (
+  CUSTOMER_ID, ADDRESS, PHONE_NO, CATEGORY, 
+  STATUS, CREATE_DATE, CREATE_USER
+) 
+VALUES 
+  (
+    'CUST001', '123, ABC Street, XYZ City', 
+    '1234567890', 'GOLD', 'ACTIVE', '2024-04-25', 
+    'John Doe'
+  ), 
+  (
+    'CUST002', '456, DEF Street, XYZ City', 
+    '2345678901', 'SILVER', 'ACTIVE', 
+    '2024-04-26', 'Jane Smith'
+  ), 
+  (
+    'CUST003', '789, GHI Street, XYZ City', 
+    '3456789012', 'BRONZE', 'ACTIVE', 
+    '2024-04-27', 'Michael Lee'
+  ), 
+  (
+    'CUST004', '012, JKL Street, XYZ City', 
+    '4567890123', 'REGULAR', 'ACTIVE', 
+    '2024-04-28', 'Alice Johnson'
+  ), 
+  (
+    'CUST005', '345, MNO Street, XYZ City', 
+    '5678901234', 'REGULAR', 'ACTIVE', 
+    '2024-04-29', 'David Williams'
+  ), 
+  (
+    'CUST006', '346, MNO Street, XYZ City', 
+    '5678901235', 'REGULAR', 'ACTIVE', 
+    '2024-04-30', 'David Willy'
+  ),
+  (
+    'CUST007', '347, MNO Street, XYZ City', 
+    '5678901236', 'RARE', 'ACTIVE', 
+    '2024-04-01', 'David William'
+  ),
+  (
+    'CUST008', '348, MNO Street, XYZ City', 
+    '5678901237', 'RARE', 'INACTIVE', 
+    '2024-04-01', 'David Will'
+  ),
+  (
+    'CUST009', '349, MNO Street, XYZ City', 
+    '5678901238', 'RARE', 'INACTIVE', 
+    '2024-04-02', 'David Wil'
+  ),
+  (
+    'CUST010', '350, MNO Street, XYZ City', 
+    '5678901239', 'RARE', 'INACTIVE', 
+    '2024-04-03', 'David Wi'
+  );
+SELECT 
+  * 
+FROM 
+  ORDER_BASE.T_CUSTOMER;
---
+++ ORDER_PROCESS/Tables/SALES_ORDER_HEADER.sql
@@ -0,0 +1,70 @@
+CREATE 
+OR REPLACE TABLE ORDER_BASE.T_SALES_ORDER_HEADER (
+  ORDER_ID VARCHAR(100), 
+  ORDER_DATE DATE, 
+  STATUS VARCHAR(100), 
+  CUSTOMER_ID VARCHAR(100), 
+  ITEM_COUNT INTEGER DEFAULT 0, 
+  TOTAL_AMOUNT NUMBER(12, 2) DEFAULT 0, 
+  DISCOUNT NUMBER(10, 2) DEFAULT 0, 
+  COUPON VARCHAR(100),
+  CREATE_DATE DATE, 
+  CREATE_USER VARCHAR(100)
+);
+INSERT INTO ORDER_BASE.T_SALES_ORDER_HEADER (
+  ORDER_ID, ORDER_DATE, STATUS, CUSTOMER_ID, 
+  COUPON, CREATE_DATE, CREATE_USER
+) 
+VALUES 
+  (
+    'ORD001', '2024-05-31', 'ACTIVE', 'CUST001', 
+    null, '2024-05-31', 'John Doe'
+  ), 
+  (
+    'ORD002', '2024-05-31', 'ACTIVE','CUST002',
+    null, '2024-05-31', 'Jane Smith'
+  ), 
+  (
+    'ORD003', '2024-05-29', 'ACTIVE', 'CUST003', 
+    null, '2024-05-29', 'Michael Lee'
+  ), 
+  (
+    'ORD004', '2024-05-29', 'ACTIVE','CUST004',
+    'COUPON_X', '2024-05-29', 'Alice Johnson'
+  ), 
+  (
+    'ORD005', '2024-05-30', 'ACTIVE', 'CUST005', 
+    'COUPON_Y', '2024-05-30', 'David Williams'
+  ),
+  (
+    'ORD006', '2024-05-30', 'ACTIVE', 'CUST006', 
+    'COUPON_Z', '2024-05-30', 'David Willy'
+  ),
+  (
+    'ORD007', '2024-05-25', 'CLOSED', 'CUST001', 
+    null, '2024-05-25', 'John Doe'
+  ), 
+  (
+    'ORD008', '2024-05-26', 'CLOSED','CUST002',
+    null, '2024-05-26', 'Jane Smith'
+  ), 
+  (
+    'ORD009', '2024-05-27', 'CLOSED', 'CUST003', 
+    null, '2024-05-27', 'Michael Lee'
+  ), 
+  (
+    'ORD0010', '2024-05-28', 'CLOSED','CUST004',
+    'COUPON_X', '2024-05-28', 'Alice Johnson'
+  ), 
+  (
+    'ORD0011', '2024-05-26', 'CLOSED', 'CUST005', 
+    'COUPON_Y', '2024-05-26', 'David Williams'
+  ),
+  (
+    'ORD0012', '2024-05-26', 'CLOSED', 'CUST006', 
+    'COUPON_Z', '2024-05-26', 'David Willy'
+  );
+SELECT 
+  * 
+FROM 
+  ORDER_BASE.T_SALES_ORDER_HEADER;
